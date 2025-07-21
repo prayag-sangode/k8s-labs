03ff36abb7e9a3b42fe92c2a8e8f7a7c354d2c88
EC2 Default User <ec2-user@devops-vm.example.com>
2025-07-21 13:36:54 +0000

    add labs

---
+++ lab4.1/certmanager-helm-install.sh
@@ -0,0 +1,11 @@
+helm repo add jetstack https://charts.jetstack.io --force-update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.15.0 \
+  --set crds.enabled=true
+helm repo ls
+helm ls
+helm -n cert-manager ls
+kubectl -n cert-manager get all
